e0b51684d60adb4c3f145f48914e8a8cdefb5137
Paolo Faccini | OSX MBP <info@paolofaccini.com>
2019-04-03 17:31:57 +0200

    Update

--- app/src/main/java/com/litlime/its_d3c/MainActivity.java
+++ app/src/main/java/com/litlime/its_d3c/MainActivity.java
@@ -2,6 +2,8 @@ package com.litlime.its_d3c;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    EditText txt1 = (EditText) findViewById(R.id.editText);
+
+    Button n1 = (Button) findViewById(R.id.num1);
+
 }
